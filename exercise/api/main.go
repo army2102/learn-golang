9f3f16058137ee71028a3966052d890ceb5eaf83
army2102 <naetirat.s@gmail.com>
2024-06-12 11:38:27 +0700

    Add auth middleware

--- exercise/api/main.go
+++ exercise/api/main.go
@@ -17,10 +17,35 @@ func main() {
 
 	app := fiber.New()
 
+	// Order sensitive
 	app.Get("/", healthCheckHandler)
-
 	app.Post("/pub", publishMessageHandler)
+
+	app.Use(authMiddleware)
 	app.Get("/get", getMessagesHandler)
 
 	app.Listen(":3000")
+}
+
+type Header struct {
+	Secret string `json:"Secret"`
+}
+
+func authMiddleware(c *fiber.Ctx) error {
+	header := new(Header)
+	if err := c.ReqHeaderParser(header); err != nil {
+		return &fiber.Error{
+			Code:    fiber.ErrBadRequest.Code,
+			Message: "Error: Cannot parse header",
+		}
+	}
+
+	if header.Secret != "1234" {
+		return &fiber.Error{
+			Code:    fiber.ErrBadRequest.Code,
+			Message: "Error: You do not have access to this route",
+		}
+	}
+
+	return c.Next()
 }
